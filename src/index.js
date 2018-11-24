4b2f1ca01a273ecbc275c32d014e5846ce39a960
william sserubiri <william.sserubiri@andela.com>
2018-11-24 18:41:41 +0300

    UI html template to show results details about a recipe

--- src/index.js
+++ src/index.js
@@ -1,17 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import configuredStore from './rootStore';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import configuredStore from './RootStore';
+import RootComponent from './RootComponent';
 
 ReactDOM.render(
   <Provider store={configuredStore()}>
-    <App />
+    <RootComponent />
   </Provider>, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
